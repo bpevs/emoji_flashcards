1f9887fbf5841dc2d4316d02db90867592e2fb83
Ben Pevsner <git@bpev.me>
2023-08-24 04:05:46 +0200

    feat: add some stuff

--- islands/Demo.tsx
+++ islands/Demo.tsx
@@ -1,25 +1,8 @@
 import type { Signal } from "@preact/signals";
 import { Button } from "../components/Button.tsx";
+import { useEffect } from "preact/hooks";
 import { supermemo, SuperMemoGrade, SuperMemoItem } from "supermemo";
 import { useComputed, useSignal } from "@preact/signals";
-
-const en_us = (await import("@/data/en_us.json", {
-  assert: { type: "json" },
-})).default;
-
-const cards = [];
-Object.keys(en_us).forEach((category) => {
-  Object.keys(en_us[category]).forEach((emojis) => {
-    cards.push({
-      emojis,
-      back: en_us[category][emojis].join("\t"),
-      interval: 0,
-      repetition: 0,
-      efactor: 2.5,
-      dueDate: (new Date()).toISOString(),
-    });
-  });
-});
 
 function practice(flashcard: Flashcard, grade: SuperMemoGrade): Flashcard {
   const { interval, repetition, efactor } = supermemo(flashcard, grade);
@@ -35,9 +18,33 @@ interface DemoProps {
 
 export default function Demo(props: DemoProps) {
   const currLang = useSignal("en_us");
+  const currSet = useSignal([]);
+
+  useEffect(async () => {
+    const charSetResp = await fetch(`/data/${currLang.value}.json`);
+    const charSet = await charSetResp.json();
+
+    const cards = [];
+    Object.keys(charSet).forEach((category) => {
+      Object.keys(charSet[category]).forEach((emojis) => {
+        cards.push({
+          emojis,
+          back: charSet[category][emojis],
+          interval: 0,
+          repetition: 0,
+          efactor: 2.5,
+          dueDate: (new Date()).toISOString(),
+        });
+      });
+    });
+    currSet.value = cards;
+  }, [currLang.value]);
+
   const isFlipped = useSignal(false);
   const currCardIndex = useSignal(0);
-  const currCard = useComputed(() => cards[currCardIndex.value]);
+  const currCard = useComputed(() => currSet.value[currCardIndex.value]);
+  const emoji = useComputed(() => currCard.value?.back[0]);
+  const hints = useComputed(() => currCard.value?.back.slice(1).join(" "));
 
   const flipButton = isFlipped.value
     ? (
@@ -51,12 +58,19 @@ export default function Demo(props: DemoProps) {
       </Button>
     )
     : <Button onClick={() => isFlipped.value = true}>flip</Button>;
-
   return (
     <div style={{ padding: "10px", textAlign: "center" }}>
       <div style={{ marginBottom: "10px", height: "20px" }}>
-        <select style={{ float: "left" }} value={currLang.value}>
-          <option>en_us</option>
+        <select
+          style={{ float: "left" }}
+          value={currLang.value}
+          onChange={(e) => {
+            currLang.value = e.target.value;
+          }}
+        >
+          <option value="en_us">English</option>
+          <option value="cn_tw">Chinese (Taiwan)</option>
+          <option value="es_es">Spanish (Spain)</option>
         </select>
       </div>
       <div
@@ -70,11 +84,14 @@ export default function Demo(props: DemoProps) {
           position: "relative",
           border: "3px solid black",
           borderRadius: "10px",
+          backgroundColor: "rgba(256,256,256, 0.5)",
         }}
       >
-        <h1>
-          {isFlipped.value ? currCard.value.back : currCard.value.emojis}
-        </h1>
+        <div>
+          <h1>{currCard.value?.emojis}</h1>
+          <h1>{isFlipped.value ? emoji.value : " "}</h1>
+          <h3>{isFlipped.value ? hints.value : " "}</h3>
+        </div>
         <div
           style={{
             position: "absolute",
@@ -87,6 +104,21 @@ export default function Demo(props: DemoProps) {
         </div>
       </div>
       <button>Download the {currLang.value} Anki deck!</button>
+
+      <table>
+        <tr>
+          <th>Emoji</th>
+          <th>Translation</th>
+        </tr>
+        {currSet.value.map(({ emojis, back }) => {
+          return (
+            <tr>
+              <td>{emojis}</td>
+              <td dir="auto">{back.join(" ")}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,16 +1,10 @@
 import Demo from "../islands/Demo.tsx";
+import { useComputed, useSignal } from "@preact/signals";
 
 export default function Home() {
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
         <h1 class="text-4xl font-bold">Emoji Language Flashcards</h1>
         <p class="my-4">
           Downloadable emoji flashcards to learn many languages
