7ab87c99a1894916c952dbe7b94ba6501123392e
Ben Pevsner <git@bpev.me>
2023-11-17 19:31:55 -0500

    feat: start moving audio to azure

--- tasks/gen_audio.ts
+++ tasks/gen_audio.ts
@@ -1,27 +1,21 @@
-import * as PlayHT from 'playht'
-import { load } from 'std/dotenv/mod.ts'
 import { ensureDir } from 'std/fs/mod.ts'
 import { listAudioFiles, readLanguageFile } from '../utilities/data_access.ts'
 import { getAudioFilename } from '../utilities/data_access_utilities.ts'
 import { GEN_DIR } from '../utilities/constants_server.ts'
 import { join } from 'std/path/mod.ts'
-import { writeAll } from 'std/streams/write_all.ts'
-import poll from '../utilities/poll_url.ts'
+import { tts } from '../utilities/tts.ts'
 
 const MAX_NOISE_LEVEL = -40
 const SILENCE_SPLIT = 1
-const SILENCE_REQUEST = 2
 const DETECT_STR = `silencedetect=noise=${MAX_NOISE_LEVEL}dB:d=${SILENCE_SPLIT}`
 const MATCH_SILENCE = /silence_start: ([\w\.]+)[\s\S]+?silence_end: ([\w\.]+)/g
-
-const env = await load()
 
 const [locale_code, inputCategoryId] = Deno.args
 if (!locale_code) throw new Error('Please supply language_code')
 
 const lang = await readLanguageFile(locale_code, true)
 const { columns, pronunciation_key } = lang
-const voice_id = lang?.meta?.play_ht?.voice_id
+const voice_id = lang?.meta?.azure?.voice_id
 if (!voice_id) throw new Error(`${locale_code} does not have a voice_id`)
 
 const pronunciationKeyIndex = columns.indexOf(pronunciation_key || '') || 0
@@ -45,107 +39,42 @@ Object.keys(emojisByCategory).forEach((category) => {
 
 console.log(locale_code, voice_id, Object.keys(emojisByCategory))
 
-const ids = await generateTranscriptionIds(emojisByCategory, voice_id)
+const ttsResults = await ttsByCategory(emojisByCategory, voice_id)
 
-console.log('source audio id: ', JSON.stringify(ids))
+console.log('source audio id: ', JSON.stringify(ttsResults))
 
-for (const idx in ids) {
-  const { categoryId, transcriptionId } = ids[idx]
-  if (!transcriptionId) {
-    console.warn(`Skipping category "${categoryId}": no transcriptionId`)
+for (const idx in ttsResults) {
+  const { categoryId, fileName } = ttsResults[idx]
+  if (!fileName) {
+    console.warn(`Skipping category "${categoryId}": no fileName`)
     continue
   }
   const emojis = emojisByCategory[categoryId]
-  const audioUrl = await downloadSourceFile(transcriptionId)
-  console.log('source audio saved: ', audioUrl)
-  await writeTranslationAudioFiles(audioUrl, locale_code, emojis)
+  console.log('source audio saved: ', fileName)
+  const filePath = join('./data/tmp', fileName)
+  await writeTranslationAudioFiles(filePath, locale_code, emojis)
 }
 
 console.log('COMPLETE!')
 Deno.exit(0)
 
-async function generateTranscriptionIds(
+async function ttsByCategory(
   emojisByCategory: { [category: string]: { [emojiKey: string]: string[] } },
   voice_id: string,
-): Promise<{ categoryId: string; transcriptionId: string }[]> {
-  PlayHT.init({ apiKey: env['PLAYHT_API_KEY'], userId: env['PLAYHT_USER_ID'] })
-
+): Promise<{ categoryId: string; fileName: string }[]> {
   return await Promise.all(
     Object.keys(emojisByCategory)
-      .filter((categoryId) => {
-        if (inputCategoryId) return categoryId === inputCategoryId
-        return true
-      })
+      .filter((catId) => !inputCategoryId || (catId === inputCategoryId))
       .map(async (categoryId) => {
-        const response = await requestSSMLAudio(
-          `<speak><p>${
-            Object.keys(emojisByCategory[categoryId])
-              .map((emojiId: string) => {
-                const emoji = emojisByCategory[categoryId][emojiId]
-                if (pronunciation_key) return emoji[pronunciationKeyIndex]
-                return emoji[0]
-              })
-              .join(`, <break time="${SILENCE_REQUEST}s"/> `)
-          }</p></speak>`,
-          voice_id,
-        )
-        if (!response.transcriptionId) console.error(response)
-        return { categoryId, transcriptionId: response.transcriptionId }
+        const texts = Object.keys(emojisByCategory[categoryId])
+          .map((emojiId: string) => {
+            const emoji = emojisByCategory[categoryId][emojiId]
+            if (pronunciation_key) return emoji[pronunciationKeyIndex]
+            return emoji[0]
+          })
+        const fileName = await tts(texts, voice_id, locale_code, categoryId)
+        return { categoryId, fileName }
       }),
-  )
-}
-
-function requestSSMLAudio(ssml: string, voice: string) {
-  try {
-    return fetch('https://api.play.ht/api/v1/convert/', {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        AUTHORIZATION: env['PLAYHT_API_KEY'],
-        'X-USER-ID': env['PLAYHT_USER_ID'],
-      },
-      body: JSON.stringify({
-        ssml: [ssml],
-        voice,
-      }),
-    }).then((res) => res.json())
-  } catch (e) {
-    console.error(e)
-  }
-}
-
-async function downloadSourceFile(transcriptionId: string) {
-  const audioResults = await getAudioResults(transcriptionId)
-  console.log(audioResults)
-  const response = await fetch(audioResults.audioUrl)
-
-  const filePath = join('./data/tmp', transcriptionId)
-
-  const file = await Deno.open(filePath, { create: true, write: true })
-
-  const arrayBuffer = new Uint8Array(await response.arrayBuffer())
-  await writeAll(file, arrayBuffer)
-
-  return filePath
-}
-
-function getAudioResults(transcriptionId: string) {
-  return poll<{ audioUrl: string; converted: boolean }>(
-    `https://api.play.ht/api/v1/articleStatus?transcriptionId=${transcriptionId}`,
-    {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        AUTHORIZATION: env['PLAYHT_API_KEY'],
-        'X-USER-ID': env['PLAYHT_USER_ID'],
-      },
-    },
-    {
-      retryLimit: 3,
-      timeBetweenPolls: 10_000,
-      predicate: ({ converted }) => converted, // "Transcription completed"
-    },
   )
 }
 
