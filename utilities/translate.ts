a39b87a2d547dc4c7547909f3a18e8fb98150417
Ben Pevsner <git@bpev.me>
2023-11-19 10:00:41 -0500

    feat: start adding azure translate

--- utilities/translate.ts
+++ utilities/translate.ts
@@ -3,31 +3,62 @@ import { load } from 'std/dotenv/mod.ts'
 const env = await load()
 
 const DEEPL_API_ENDPOINT = 'https://api-free.deepl.com/v2/translate'
-const DEEPL_API_KEY = env['DEEPL_API_KEY']
+const AZURE_API_ENDPOINT =
+  'https://api.cognitive.microsofttranslator.com/translate'
 
-async function translateDeepl(
+async function translateAzure(
   texts: string[],
   targetLanguage: string,
 ): Promise<string[]> {
-  const combinedText = texts.join('\n')
-  const text = encodeURIComponent(combinedText)
-  const target_lang = targetLanguage.split('-')[0]
+  const text = texts.join('\n')
+  const url = new URL(AZURE_API_ENDPOINT)
+  url.search = new URLSearchParams({
+    'api-version': '3.0',
+    from: 'en',
+    to: JSON.stringify([targetLanguage]),
+  }).toString()
 
-  const response = await fetch(DEEPL_API_ENDPOINT, {
+  const response = await fetch(url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: `auth_key=${DEEPL_API_KEY}&text=${text}&target_lang=${target_lang}`,
+    headers: {
+      'Ocp-Apim-Subscription-Key': env['AZURE_TRANSLATE_KEY'],
+      'Ocp-Apim-Subscription-Region': env['AZURE_TRANSLATE_LOCATION'],
+      'Content-type': 'application/json',
+      'X-ClientTraceId': crypto.randomUUID(),
+    },
+    body: JSON.stringify([{ text }]),
   })
 
   const result = await response.json()
   return result.translations[0].text.split('\n')
 }
 
-export const _internals = { translateDeepl }
+async function translateDeepl(
+  texts: string[],
+  target_lang: string,
+): Promise<string[]> {
+  const text = texts.join('\n')
+  const response = await fetch(DEEPL_API_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: env['DEEPL_API_KEY'],
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text, target_lang }),
+  })
+
+  const result = await response.json()
+  return result.translations[0].text.split('\n')
+}
+
+export const _internals = { translateAzure, translateDeepl }
 
 export const translate = (
   texts: string[],
   targetLanguage: string,
+  api = 'DEEPL',
 ): Promise<string[]> => {
-  return _internals.translateDeepl(texts, targetLanguage)
+  if (api === 'AZURE') return _internals.translateAzure(texts, targetLanguage)
+  if (api === 'DEEPL') return _internals.translateDeepl(texts, targetLanguage)
+  else throw new Error(`Does not support the transation api ${api}`)
 }
