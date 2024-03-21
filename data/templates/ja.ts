afef9d81e205e8d64adac7b3ef450316dec66ccb
Ben Pevsner <git@bpev.me>
2024-03-20 20:40:08 -0600

    feat: update ja template for hiragana

--- data/templates/ja.ts
+++ data/templates/ja.ts
@@ -1,23 +1,23 @@
 import { Template } from '@flashcard/core'
 import { hintButton, hintDisplay } from './default.js'
 
-const romajiButton = hintButton.replace(/{{hint}}/g, '{{romaji}}')
-const romajiDisplay = hintDisplay.replace(/{{hint}}/g, '{{romaji}}')
+const hiraganaButton = hintButton.replace(/{{hint}}/g, '{{hiragana}}')
+const hiraganaDisplay = hintDisplay.replace(/{{hint}}/g, '{{hiragana}}')
 
 export default [
   new Template(
     'Listening',
-    `{{audio}}<br>${romajiButton}${romajiDisplay}`,
-    `{{FrontSide}}<hr id=answer><h1>{{emoji}}</h1><br>{{text}} ({{romaji}})`,
+    `{{audio}}<br>${hiraganaButton}${hiraganaDisplay}`,
+    `{{FrontSide}}<hr id=answer><h1>{{emoji}}</h1><br>{{text}} ({{hiragana}})`,
   ),
   new Template(
     'Reading',
     `{{text}}`,
-    `{{FrontSide}}<hr id=answer><h1>{{emoji}}</h1><br>{{audio}}<br>{{romaji}}`,
+    `{{FrontSide}}<hr id=answer><h1>{{emoji}}</h1><br>{{audio}}<br>{{hiragana}}`,
   ),
   new Template(
     'Speaking',
     `<h1>{{emoji}}</h1>`,
-    `{{FrontSide}}<hr id=answer>{{audio}}<br>${romajiButton}<br>{{text}} ({{romaji}})`,
+    `{{FrontSide}}<hr id=answer>{{audio}}<br>${hiraganaButton}<br>{{text}} ({{hiragana}})`,
   ),
 ]
